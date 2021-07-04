f48b3659f33b858af9b91935842428d1e4cedf47
Saimun <saimun4u@gmail.com>
2021-07-03 21:03:58 -0500

    Arrow to move through the images added

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 // Button listeners
 
 nextBtn.addEventListener('click', ()=> {
+    if (counter >= carouselImages.length - 1) return;
     carouselSlide.style.transition = 'transform 0.4s ease-in-out';
     counter++;
     console.log(counter)
@@ -25,7 +26,24 @@ nextBtn.addEventListener('click', ()=> {
 })
 
 prevBtn.addEventListener('click', ()=> {
+    if (counter <= 0) return;
     carouselSlide.style.transition = 'transform 0.4s ease-in-out';
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+})
+
+
+carouselSlide.addEventListener('transitionend', ()=>{
+    console.log(carouselImages[counter]);
+    if (carouselImages[counter].id === "lastClone"){
+        carouselSlide.style.transition = 'none';
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+
+    if (carouselImages[counter].id === "firstClone"){
+        carouselSlide.style.transition = 'none';
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
 })
